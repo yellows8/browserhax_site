f3e6952d87c10463a0c4284c836480a02c7b804e
yellows8 <yellows8@users.noreply.github.com>
2016-07-09 16:23:56 -0400

    More changes for next release.

--- 3dsbrowserhax.php
+++ 3dsbrowserhax.php
@@ -30,10 +30,8 @@ if($_SERVER['SCRIPT_NAME'] == "/3dsbrowserhax_auto.php")
 		}
 
 		header("Location: http://".$_SERVER['SERVER_NAME']."/$page$appendstr");
-		exit;
 	}
-
-	if(strstr($ua, "Mozilla/5.0 (New Nintendo 3DS"))
+	else if(strstr($ua, "Mozilla/5.0 (New Nintendo 3DS"))
 	{
 		if(strstr($ua, "1.0.9934") || strstr($ua, "1.1.9996") || strstr($ua, "1.2.10085") || strstr($ua, "1.3.10126"))
 		{
@@ -49,13 +47,16 @@ if($_SERVER['SCRIPT_NAME'] == "/3dsbrowserhax_auto.php")
 		}
 
 		echo "<html><head><script>window.location.assign(\"http://".$_SERVER['SERVER_NAME']."/$page$appendstr\");</script></head><body></body></html>";
-		exit;
 	}
+
+	exit;
 }
 
 $con = "<head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" /><title>Nintendo 3DS web-browser exploits</title></head>\n<body>";
 
-$con.= "The following are system-web-browser exploits for Nintendo 3DS, the ones hosted here are for booting the *hax <a href=\"https://smealum.github.io/3ds/\">payloads</a>. See the linked repos for usage details and supported browser versions, etc, including the common codebase: <a href=\"https://github.com/yellows8/3ds_browserhax_common\">https://github.com/yellows8/3ds_browserhax_common</a>. On a supported browser, you can go <a href=\"http://".$_SERVER['SERVER_NAME']."/3dsbrowserhax_auto.php\">here</a>, which will automatically determine which exploit to return for your browser.<br/>\n  Once you boot into the payload successfully, you can then install exploit(s) from <a href=\"https://www.3dbrew.org/wiki/Homebrew_Exploits\">here</a>(this is <b>highly</b> recommended). This highly recommended because of the browser-version-check implemented with v9.9, see <a href=\"https://www.3dbrew.org/wiki/Internet_Browser\">here</a> for details.<br/>\n<br/>\nThe following QR-code can be scanned by an Old3DS/New3DS from the Home Menu camera menu, for accessing the previously mentioned <a href=\"http://".$_SERVER['SERVER_NAME']."/3dsbrowserhax_auto.php\">URL</a>: <br/>\n<img src=\"3dsbrowserhax_auto_qrcode.png\"><br/>\n<br/>\n";
+$con.= "The following are system-web-browser exploits for Nintendo 3DS, the ones hosted here are for booting the *hax <a href=\"https://smealum.github.io/3ds/\">payloads</a>. On a supported browser, you can go <a href=\"http://".$_SERVER['SERVER_NAME']."/3dsbrowserhax_auto.php\">here</a>, which will automatically determine which exploit to return for your browser.<br/>\n  Once you boot into the payload successfully, you can then install exploit(s) from <a href=\"https://www.3dbrew.org/wiki/Homebrew_Exploits\">here</a>(this is <b>highly</b> recommended). This highly recommended because of the browser-version-check implemented with v9.9, see <a href=\"https://www.3dbrew.org/wiki/Internet_Browser\">here</a> for details.<br/>\n<br/>\nThe following QR-code can be scanned by an Old3DS/New3DS from the Home Menu camera menu, for accessing the auto <a href=\"http://".$_SERVER['SERVER_NAME']."/3dsbrowserhax_auto.php\">page</a>: <br/>\n<img src=\"3dsbrowserhax_auto_qrcode.png\"><br/>\n<br/>\n";
+
+$con.= "Regarding source repos, the common codebase is located <a href=\"https://github.com/yellows8/3ds_browserhax_common\">here</a>, and the source of this site is located <a href=\"https://github.com/yellows8/browserhax_site\">here</a>. See below for the exploit repos.<br/><br/>\n";
 
 $con.= "Note that systems where the system was updated with a >=v9.9 gamecard sysupdate have a dummy browser installed, unless an online sysupdate was done afterwards. See <a href=\"https://www.3dbrew.org/wiki/Internet_Browser\">here</a> for details. The system-version for this is 9.9.0-X, where X is less than 26.<br/><br/>\n";
 
@@ -78,7 +79,7 @@ $con.= "</ol><br/>\n";
 
 if(file_exists("3dsbrowserhax_siteincpage.php"))require_once("3dsbrowserhax_siteincpage.php");
 
-$con.= "Due to the *hax payloads, the exploits hosted here are only usable on systems with system-version >=v9.0 with the system web-browser installed.<br/>Using the <a href=\"http://".$_SERVER['SERVER_NAME']."/3dsbrowserhax_auto.php\">auto</a> page(such as with the above QR-code) instead of going to the exploit pages linked below manually, is <b>highly</b> recommended.<br/><br/>\n";
+$con.= "Due to the *hax payloads, the exploits hosted here are only usable on systems with system-version >=v9.0 with the system web-browser installed.<br/><br/>Using the <a href=\"http://".$_SERVER['SERVER_NAME']."/3dsbrowserhax_auto.php\">auto</a> page(such as with the above QR-code) instead of going to the exploit pages linked below manually, is <b>highly</b> recommended.<br/><br/>\n";
 
 $con.= "<b>Please note that Old3DS-browserhax is much more unstable starting with 10.4.0-29. When it fails at an orange-screen(or afterwards) native code is already running, therefore there's nothing more you can do other than rebooting the system and trying again. Hence, as already said above installing other hax from here is highly recommended. See <a href=\"https://github.com/yellows8/3ds_browserhax_common\">here</a> regarding crashes/hangs/screen-colors.</b><br/><br/>\n";
 
@@ -90,14 +91,17 @@ $con.= "<tr>
   <th>Supported system-versions</th>
   <th>Fixed with system-version</th>
   <th>Returned by the auto-page</th>
+  <th>Exploit runs automatically</th>
   <th>Repo</th>
 </tr>\n";
 
-$con.= "<tr><td>Old3DS</td><td><a href=\"http://".$_SERVER['SERVER_NAME']."/sliderhax.php\">sliderhax</a></td><td>All <=10.1.0-27(aka <=X.X.X-27)</td><td>>=10.2.0-28(aka >=X.X.X-28)</td><td>Only when the browser version is supported.</td><td><a href=\"https://github.com/yellows8/3ds_webkithax\">Here</a></td></tr>\n";
-$con.= "<tr><td>New3DS</td><td><a href=\"http://".$_SERVER['SERVER_NAME']."/3dswebkithax_removewinframe.php\">3dswebkithax_removewinframe</a></td><td>All <=9.8.0-25(aka <=X.X.X-25)</td><td>>=9.9.0-26(aka >=X.X.X-26)</td><td>Not returned at all(this exploit is also very unreliable).</td><td><a href=\"https://github.com/yellows8/3ds_webkithax\">Here</a></td></tr>\n";
-$con.= "<tr><td>New3DS</td><td><a href=\"http://".$_SERVER['SERVER_NAME']."/browserhax_fright.php\">browserhax_fright</a></td><td>All <=10.1.0-27(aka <=X.X.X-27)</td><td>>=10.2.0-28(aka >=X.X.X-28)</td><td>Only when the browser version is supported by this exploit.</td><td><a href=\"https://github.com/yellows8/browserhax_fright\">Here</a></td></tr>\n";
-$con.= "<tr><td>New3DS</td><td><a href=\"http://".$_SERVER['SERVER_NAME']."/browserhax_fright_tx3g.php\">browserhax_fright_tx3g</a></td><td>All <=10.5.0-30(aka <=X.X.X-30)</td><td>>=10.6.0-31(aka >=X.X.X-31)</td><td>Only when the browser version isn't supported by the above browserhax_fright.</td><td><a href=\"https://github.com/yellows8/browserhax_fright\">Here</a></td></tr>\n";
-$con.= "<tr><td>Old3DS</td><td><a href=\"http://".$_SERVER['SERVER_NAME']."/spider28hax.php\">spider28hax</a></td><td>Only 10.2.0-28-10.5.0-30</td><td>>=10.6.0-31(aka >=X.X.X-31)</td><td>Only when the browser version isn't supported by sliderhax.</td><td><a href=\"https://github.com/yellows8/3ds_webkithax\">Here</a></td></tr>\n";
+$con.= "<tr><td>Old3DS</td><td><a href=\"http://".$_SERVER['SERVER_NAME']."/sliderhax.php\">sliderhax</a></td><td>All <=10.1.0-27(aka <=X.X.X-27)</td><td>>=10.2.0-28(aka >=X.X.X-28)</td><td>Only when the browser version is supported.</td><td>No, see repo.</td><td><a href=\"https://github.com/yellows8/3ds_webkithax\">Here</a></td></tr>\n";
+$con.= "<tr><td>New3DS</td><td><a href=\"http://".$_SERVER['SERVER_NAME']."/3dswebkithax_removewinframe.php\">3dswebkithax_removewinframe</a></td><td>All <=9.8.0-25(aka <=X.X.X-25)</td><td>>=9.9.0-26(aka >=X.X.X-26)</td><td>Not returned at all(this exploit is also very unreliable).</td><td>Yes</td><td><a href=\"https://github.com/yellows8/3ds_webkithax\">Here</a></td></tr>\n";
+$con.= "<tr><td>New3DS</td><td><a href=\"http://".$_SERVER['SERVER_NAME']."/browserhax_fright.php\">browserhax_fright</a></td><td>All <=10.1.0-27(aka <=X.X.X-27)</td><td>>=10.2.0-28(aka >=X.X.X-28)</td><td>Only when the browser version is supported by this exploit.</td><td>Yes</td><td><a href=\"https://github.com/yellows8/browserhax_fright\">Here</a></td></tr>\n";
+$con.= "<tr><td>New3DS</td><td><a href=\"http://".$_SERVER['SERVER_NAME']."/browserhax_fright_tx3g.php\">browserhax_fright_tx3g</a></td><td>All <=10.5.0-30(aka <=X.X.X-30)</td><td>>=10.6.0-31(aka >=X.X.X-31)</td><td>Only when the browser version isn't supported by older exploit(s).</td><td>Yes</td><td><a href=\"https://github.com/yellows8/browserhax_fright\">Here</a></td></tr>\n";
+$con.= "<tr><td>Old3DS</td><td><a href=\"http://".$_SERVER['SERVER_NAME']."/spider28hax.php\">spider28hax</a></td><td>Only 10.2.0-28-10.5.0-30(aka X.X.X-28-X.X.X-30)</td><td>>=10.6.0-31(aka >=X.X.X-31)</td><td>Only when the browser version isn't supported by older exploit(s).</td><td>Yes</td><td><a href=\"https://github.com/yellows8/3ds_webkithax\">Here</a></td></tr>\n";
+$con.= "<tr><td>Old3DS</td><td><a href=\"http://".$_SERVER['SERVER_NAME']."/spider31hax.php\">spider31hax</a></td><td>Only 10.6.0-31-11.0.0-33(aka X.X.X-31-X.X.X-33)</td><td>None</td><td>Only when the browser version isn't supported by older exploit(s).</td><td>Yes</td><td><a href=\"https://github.com/yellows8/3ds_webkithax\">Here</a></td></tr>\n";
+$con.= "<tr><td>New3DS</td><td><a href=\"http://".$_SERVER['SERVER_NAME']."/skater31hax.php\">skater31hax</a></td><td>All <=11.0.0-33(aka <=X.X.X-33)</td><td>None</td><td>Only when the browser version isn't supported by older exploit(s).</td><td>Yes</td><td><a href=\"https://github.com/yellows8/browserhax_fright\">Here</a></td></tr>\n";
 
 $con.= "</table>";
 
